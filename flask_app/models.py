7b331e7bf96f7fe3016ffd52e19086331289e4ca
Peter Prescott <prescott.peter@gmail.com>
2021-11-29 14:03:19 +0000

    Move db again

--- flask_app/models.py
+++ flask_app/models.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (scoped_session, sessionmaker, relationship,
                             backref)
 
-engine = create_engine('sqlite:///../db.sqlite3')
+engine = create_engine('sqlite:///db.sqlite3')
 db_session = scoped_session(sessionmaker(autocommit=False,
     autoflush=False, bind=engine))
 
